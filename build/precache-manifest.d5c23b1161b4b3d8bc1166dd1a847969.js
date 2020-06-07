d9af13ea03113462db729a6ef18ebe8301bf532a
_CHAI__ <apichai.freedom@gmail.com>
2020-06-07 15:04:06 +0700

    test

---
+++ build/precache-manifest.d5c23b1161b4b3d8bc1166dd1a847969.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f02cdb966556db7d406ca429fe765f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd4e6c704eb629b4588d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4782e1f47ab8f5416ec",
+    "url": "/static/js/2.98c95162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.98c95162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4e6c704eb629b4588d",
+    "url": "/static/js/main.acde6c1d.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
